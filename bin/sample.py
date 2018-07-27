4ffa84414970f258580f2fcb73d8f54565cac389
Jose Licon <licon@math.uni-koeln.de>
2018-07-27 19:32:18 +0200

    Adapted sampling bin scrptpt

--- bin/sample.py
+++ bin/sample.py
@@ -6,6 +6,7 @@ import argparse
 import os
 # from homology.util.sample import *
 from homology.sampler import Sampler
+from homology.util.sample import project_2d_cloud_field
 
 def process_arguments():
 
@@ -68,18 +69,17 @@ def main():
     for t in timerange:
         print '********* Timestep: %d *********' % t
         if args.project:
-            raise NotImplementedError
             # Get two-dimensional projected field
             # domain = project_2d_cloud_field(dataset, t, args.varname)
 
-            # # Get the required number of samples and write to disk
-            # for i in range(args.n):
-            #     sample = sample_connected_components(domain, tmp, args.sample_ratio)
-            #     if sample is None:
-            #         continue
-            #     else:
-            #         out_fname = '%s/t%d_sample%d' % (args.output_dir, t, i+1)
-            #         save_sample(dataset, sample, out_fname)
+            # Get the required number of samples and write to disk
+            for i in range(args.n):
+                sample = sample_connected_components(domain, tmp, args.sample_ratio)
+                if sample is None:
+                    continue
+                else:
+                    out_fname = '%s/t%d_sample%d' % (args.output_dir, t, i+1)
+                    save_sample(dataset, sample, out_fname)
         else:
             # Assume the samples will be taken in horizontal planes
             # assert dataset.variables[args.varname].dimensions[1] == 'zm'
